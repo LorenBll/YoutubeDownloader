4375d230ba1b9cd49af7740c66f9b1d27f9975e2
LorenBll <91012830+LorenBll@users.noreply.github.com>
2026-07-20 00:44:13 +0200

    audit: fix README inaccuracies and remove dead code

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,3 @@
 """Model classes for YoutubeDownloader."""
 
-from .get_request import GetRequest
-from .get_response import GetResponse
-from .post_request import PostRequest
 from .post_response import PostResponse
